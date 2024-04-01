62861ceb6bdd74101d46924e5a2f402c1783f91a
Mustafa Önder ÜNLÜ <mustafaonderunluu1@gmail.com>
2024-04-01 22:45:34 +0300

    Proje dosyası ekle.

---
+++ BasitUygulama/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasitUygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                                                      Baklava Dilimi ");
+            Console.WriteLine("Boyut değeri giriniz : ");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i < boyut; i++) {
+
+                for (int k = boyut; k >= i; k--) {
+
+                    Console.Write(" ");
+
+
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = (boyut-1); i >= 1; i--)
+            {
+
+                for (int k = boyut; k >= i; k--)
+                {
+
+                    Console.Write(" ");
+
+
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+          
+            Console.ReadKey();
+            
+
+
+
+        }
+    }
+}
